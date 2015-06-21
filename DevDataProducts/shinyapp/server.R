e066fd87b20d75e1dc9c479f062e4d7b07b56ac6
coursera-2015 <rodbass84-coursera@yahoo.com>
2015-06-21 00:37:35 -0400

    Finished app

--- DevDataProducts/shinyapp/server.R
+++ DevDataProducts/shinyapp/server.R
@@ -1,16 +1,25 @@
-library(UsingR)
-data(galton)
+library(shiny)
 
 shinyServer(
     function(input, output) {
-        output$myHist <- renderPlot({
-            hist(galton$child, xlab='child height', col='lightblue',main='Histogram')
-            mu <- input$mu
-            lines(c(mu, mu), c(0, 200),col="red",lwd=5)
-            mse <- mean((galton$child - mu)^2)
-            text(63, 150, paste("mu = ", mu))
-            text(63, 140, paste("MSE = ", round(mse, 2)))
+
+        paymentCalculator <- reactive({
+            invoicePrice = input$msrp - input$discount
+            moneyFactor = input$interestRate / 2400
+            residual = input$residualValue / 100
+            
+            # Step 1: what will the car be worth
+            futureValue = input$msrp * residual
+            
+            # Step 2: leased value of the car
+            leasedValue = invoicePrice - futureValue
+            
+            # Step 3: monthly lease payment
+            leasePayment = leasedValue / input$leaseTerm + (invoicePrice + futureValue) * moneyFactor        
+            paste("Your payment is $", round(leasePayment, 2), ".", sep = "")
         })
-        
-    }
-)
+    
+        output$paymentText = renderText({
+            paymentCalculator()
+        })
+    })
--- DevDataProducts/shinyapp/ui.R
+++ DevDataProducts/shinyapp/ui.R
@@ -1,11 +1,25 @@
 library(shiny)
 
 shinyUI(pageWithSidebar(
-    headerPanel("Example plot"),
+    headerPanel("Car Lease Payment Calculator"),
     sidebarPanel(
-        sliderInput('mu', 'Guess at the mu',value = 70, min = 60, max = 80, step = 0.05,)
+        helpText("Adjust the values to determine the payment amount."),
+
+        sliderInput('msrp', 'MSRP of the vehicle ($)', 
+                    value = 23000, min = 15000, max = 50000, step = 1000,),
+        sliderInput('interestRate', 'Interest rate for the lease (%)', 
+                    value = 3, min = 1, max = 7, step = .25,),
+        sliderInput('leaseTerm', 'Lease term (months)', 
+                    value = 36, min = 12, max = 60, step = 6,),
+        sliderInput('residualValue', 'Residual value of the car (%)', 
+                    value = 57, min = 30, max = 70, step = -1)
     ),
     mainPanel(
-        plotOutput('myHist')
+        numericInput("discount", "Dealer discount, incentive, down payment or trade-in amount ($, optional)", 3000),
+        
+        h3(textOutput("paymentText")),
+        br(),
+        helpText("Note: The discount amount must be less than the MSRP,",
+                "the payment will not be correct otherwise (could be negative).")
     )
 ))
